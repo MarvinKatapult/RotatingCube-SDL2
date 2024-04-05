671c5eb057b9d72f8b6093e7a9815d3ec7936e3f
MarvinKatapult <ibingmarvin@gmail.com>
2024-04-05 16:43:53 +0200

    Restructured Project

---
+++ build.sh
@@ -0,0 +1,33 @@
+
+project="RotatingCube-SDL2"
+
+red_echo() {
+    local text=$1
+    echo -e "\e[31m$text\e[0m"
+}
+
+green_echo() {
+    local text=$1
+    echo -e "\e[32m$text\e[0m"
+}
+
+yellow_echo() {
+    local text=$1
+    echo -e "\e[33m$text\e[0m"
+}
+
+########## Compiling Binary ##########
+    echo -n "Compiling all files for project "
+    yellow_echo "$project"
+######### Add Source Files #########
+    g++ -o $project -Wextra -Wall -pedantic -g -Iinclude -lSDL2 \
+        src/main.cpp \
+        src/mmatrix.cpp
+###################################
+    error_code=$?
+    echo -n "Compiling "
+    if [ $error_code -eq 0 ]; then
+        green_echo "finished" 
+    else
+        red_echo "failed" 
+    fi
